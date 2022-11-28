29670ed4a8099b3026f6081e1905c7c8b9fb3c34
Mouad <1668934@uab.cat>
2022-11-28 23:26:06 +0100

    Primera càrrega

---
+++ exercici3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (){
+int a,b=1,c=2,d=3;
+a=b+c*d;
+a=(b+c)*d;
+d=a;
+return 0;
+}
---
+++ exercici4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main (){
+int a=3,b;
+char c='a';
+-----------------------------------
+int a=3;
+char c='a',b;
+b=a*c;
+return 0;
+}
